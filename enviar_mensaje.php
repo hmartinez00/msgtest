40673a77964f89884a1a2de82b41747b112b42ec
hmartinez00 <hectoralonzomartinez00@gmail.com>
2024-10-10 23:02:51 -0400

    commit 241010 2302

--- enviar_mensaje.php
+++ enviar_mensaje.php
@@ -3,6 +3,9 @@ require_once 'vendor/autoload.php';
 
 use TelegramBot\Api\BotApi;
 
+$ruta_archivo = 'output.text';
+$contenido = file_get_contents($ruta_archivo);
+
 $directory = 'setting.json';
 $directoryData = json_decode(file_get_contents($directory));
 $telegram = $directoryData->telegram;
@@ -10,8 +13,10 @@ $telegram = $directoryData->telegram;
 $TU_TOKEN_BOT=$telegram[0];
 $ID_DE_TU_CHAT=$telegram[1];
 $fecha_hora = date('Y-m-d H:i:s');
-$text = '[' . $fecha_hora . '] ' . 
-    'Actualizacon ejecutada!';
+$text = '[' . $fecha_hora . '] ' .
+    'Actualizacion ejecutada!' . "\n\n" .
+    $contenido
+    ;
 
 // Reemplaza 'TU_TOKEN_BOT' con tu token real
 $telegram = new BotApi($TU_TOKEN_BOT);
@@ -21,4 +26,8 @@ $telegram->sendMessage(
     $ID_DE_TU_CHAT, // Reemplaza con el ID de tu chat
     $text,
 );
+
+$git_log = shell_exec('git log');
+
+echo $git_log;
 
