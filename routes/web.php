5f26b401f686116516156258de24230e6a6acabc
hmartinez00 <hectoralonzomartinez00@gmail.com>
2024-10-12 08:17:09 -0400

    Actualizacon de web rootes post breeze install

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MsgController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -15,6 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    Route::get('/home', [MsgController::class, 'home'])->name('home');
+    Route::post('/telegram', [MsgController::class, 'telegramhandle']);
 });
 
 require __DIR__.'/auth.php';
